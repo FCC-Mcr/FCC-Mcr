4ac87846cf740446be13bf5138a894fa2636647f
James R. Davenport <jrdavenport85@gmail.com>
2016-05-16 20:08:09 +0000

    fix capitalisation of component files, basic react router with example routes in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,42 @@ require('./styles/main.sass') // root stylesheeet - .css, .scss or .sass
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Component from './components/component';
 
-ReactDOM.render(<Component />, document.querySelector('.container'));
+
+import Component from './components/Component';
+
+import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router'
+
+
+const TestMenus = React.createClass({ 
+    
+    render() {
+        return (
+            <div>
+              <h1>im a menu component</h1>
+              
+                { React.cloneElement(this.props.children, this.props) }
+              
+            </div>
+            )
+    }
+    
+});
+    
+const TestComp = () => (<div>testdivpleaseignore<Link to="/">link to home</Link></div>)
+const TestHome = () => (<div>testhomepage<Link to="/test">link to page</Link></div>)
+
+const MainRouter = () => (
+  (
+    <Router history={browserHistory}>
+      <Route path="/" component={ TestMenus }>
+        <IndexRoute component={ TestHome }>test</IndexRoute>
+        <Route path='/test' component={TestComp}></Route>
+      </Route>
+    </Router>
+  )    
+)
+
+
+
+ReactDOM.render(<MainRouter/>, document.querySelector('.container'));
