5753da1d341c6d247edfea6b68f2ae51384a9c79
JD-aka-Techy <jrdavenport85@gmail.com>
2016-06-15 21:25:25 +0100

    basic contact page

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -6,7 +6,26 @@ import React from 'react';
 const ContactPage = () => (
   (
     <div className="fill">
-      Contact page
+      <div className="content-box">
+        FreeCodeCamp Manchester meet monthly at the <a href="https://codeupmcr.co.uk">
+          CodeUp Manchester
+        </a> meetUp. Alternatively you can reach us on our slack channel.
+        <div className="row contact-links">
+          <div className="col col-xs-6">
+            <a href="https://fcc-mcr-invite.herokuapp.com/">
+              <i className="fa fa-slack"></i>
+              slack
+            </a>
+          </div>
+          <div className="col col-xs-6">
+            <a href="https://codeupmcr.co.uk">
+              <i className="fa fa-code"></i>
+              CodeUp
+            </a>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 );
