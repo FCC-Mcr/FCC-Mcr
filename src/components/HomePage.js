8056ff1b1d136d7659cab552379638bb8744f4d9
James R. Davenport <jrdavenport85@gmail.com>
2016-06-29 20:09:49 +0000

    base home page layout and styles

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router';
 /*
   A fixed aspect ratio box on the home page
+  types => square, title, wide
 */
-const HomePageBox = ( {content, linkUrl} ) => (
+const HomePageBox = ({ linkUrl, type, children }) => (
   (
-    <div className="menu-square-container">
-      <Link className="fill" to="/about">
-        <div to={ linkUrl } className="menu-square">
-          <span>{ content }</span>
+    <div className={"box-" + type}>
+      <Link className="fill" to={linkUrl}>
+        <div className="hero">
+          { children }
         </div>
       </Link>
     </div>
@@ -22,7 +23,25 @@ const HomePageBox = ( {content, linkUrl} ) => (
 const HomePage = () => (
   (
     <div className="fill home-page">
-      <HomePageBox content={"ABOUT US"} linkUrl={"/about"} />
+      <div className="home-wrap">
+      
+        <HomePageBox linkUrl="/" type="title">
+          <span>Free Code Camp Manchester</span>
+        </HomePageBox>
+        
+        <HomePageBox content="about us" linkUrl="/about" type="square">
+          <span>about us</span>
+        </HomePageBox>
+        
+        <HomePageBox content="contact" linkUrl="/contact" type="square">
+          <span>contact</span>
+        </HomePageBox>
+
+        <HomePageBox content="learning resources" linkUrl="/resources" type="wide">
+          <span>learning resources</span>
+        </HomePageBox>
+
+      </div>
       
     </div>
   )
