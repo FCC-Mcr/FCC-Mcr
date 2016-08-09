0d80ab7a31433c859ac3faeade469cec8aa7c803
James R. Davenport <jrdavenport85@gmail.com>
2016-08-09 15:16:49 +0000

    style tweaks

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -7,13 +7,12 @@ const AboutPage = () => (
   (
     <div className="fill">
       <div className="about-banner">
-        <img src="./src/assets/snoo.png"  />
-        banner image here.
+        <img src="./src/assets/first.jpg"  />
       </div>
       <div className="content-box">
         <h3>We are...</h3>
         <p>
-          A collection of coders and Members of the Manchester FreeCodeCamp
+          A collection of Learners, Coders and Members of the Manchester FreeCodeCamp
            users group working together to:
         </p>
         <ul>
@@ -21,6 +20,7 @@ const AboutPage = () => (
           <li>Share Resources.</li>
           <li>Progress along the FCC curriculum.</li>
           <li>Learn.</li>
+          <li>become better programmers be it for fun or a change of career.</li>
         </ul>
         <p>
           The purpose of this site is to further pursue those goals.
--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ResourceListItem from './ResourceListItem';
 
-
-
 const ResourceList = ({ resources = [] }) => (
   <ul>
     {
