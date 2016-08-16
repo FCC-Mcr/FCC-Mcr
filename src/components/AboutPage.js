d3498a4fd77256e0dfaecad3be5551b416ab7671
Adam Marc Williams <adam@adammarcwilliams.co.uk>
2016-08-16 10:28:14 +0000

    Styled ABOUT page, added font-stack & refactored SASS structure

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -6,10 +6,9 @@ import React from 'react';
 const AboutPage = () => (
   (
     <div className="fill">
-      <div className="about-banner">
-        <img src="./src/assets/first.jpg"  />
-      </div>
+      <div className="about-banner"></div>
       <div className="content-box">
+      <div className="content-box--left">
         <h3>We are...</h3>
         <p>
           A collection of Learners, Coders and Members of the Manchester FreeCodeCamp
@@ -18,13 +17,26 @@ const AboutPage = () => (
         <ul>
           <li>Build things.</li>
           <li>Share Resources.</li>
+          <li>Learn cool stuff.</li>
           <li>Progress along the FCC curriculum.</li>
-          <li>Learn.</li>
-          <li>become better programmers be it for fun or a change of career.</li>
+          <li>Become better programmers, be it for fun or a change of career.</li>
         </ul>
+        <br></br>
         <p>
-          The purpose of this site is to further pursue those goals.
+          <strong>We'd love for you to join us in the pursuit of these goals.</strong>
         </p>
+        <a href="https://fcc-mcr-invite.herokuapp.com/">
+          <i className="fa fa-slack"></i>
+          slack
+        </a>
+        <a href="https://codeupmcr.co.uk">
+          <i className="fa fa-code"></i>
+          CodeUp
+        </a>
+      </div>
+      <div className="content-box--right">
+        <img className="about-image" src="/src/assets/first.jpg" alt="FCCMrc study group in session at Code-Up"></img>
+      </div>
       </div>
     </div>
   )
